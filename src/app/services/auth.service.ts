9378f3f10fcd9978c3410de576b5ca7fdd11ccbd
IvanNavi13 <ivannavi.13@hotmail.com>
2022-09-28 04:28:33 -0500

    Ultima version, interceptores

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -5,13 +5,17 @@ import { Injectable } from '@angular/core';
 })
 export class AuthService {
 
-  public auth: boolean = false;
+  public auth : boolean = false;
+  public username! : string;
+  public password! : string;
+  public token : string = '';
   
   constructor() { }
 
   login(){
     this.auth = true;
     localStorage.setItem('auth', this.auth.toString())
+    // this.user = 
   }
 
   logout(){
@@ -30,5 +34,21 @@ export class AuthService {
   /**
    * Se puede colocar en el constructor, en el componente prinipal o en un guard.  Todo depende de la regla de negocio que se aplique
    */
+
+   saveSession(username : string, password : string) {
+    // localStorage.setItem('username', username);
+    // localStorage.setItem('password', password);
+    this.username = username;
+    this.password = password;
+  }
+
+  getToken(username : string, password : string) : boolean {
+    if (username == 'fer' && password == '123')
+    {
+      this.token = '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';
+    }
+
+    return (this.token != '');
+  }
 
 }
